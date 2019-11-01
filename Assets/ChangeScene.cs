498761113a10b4e198e4485d9f93d8dd3bd10cfc
SebastianB <seba23495@gmail.com>
2019-11-01 16:48:20 +0100

    Menu scene and Start/Exit buttons has been added

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+
+    // Change To Scene
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+
+      ///  Application.LoadLevel(sceneToChangeTo);
+       
+        SceneManager.LoadScene(sceneToChangeTo);
+
+    }
+}
