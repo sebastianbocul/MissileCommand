6c50191b836cf7aa5226b2f2d0b9d30105ed332e
sebastian bocul <seba23495@gmail.com>
2019-11-04 13:20:55 +0100

    Restart highScores in menu added

---
+++ Assets/highScoreUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class highScoreUpdate : MonoBehaviour
+{
+
+    public GameObject highScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScore.GetComponent<TextMesh>().text = PlayerPrefs.GetInt("HighScore").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/restartScores.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartScores : MonoBehaviour
+{
+
+    public GameObject highScore;
+    
+    public void restartScore()
+    {
+      //  PlayerPrefs.DeleteKey("HighScores");
+        PlayerPrefs.DeleteAll();
+        highScore.GetComponent<TextMesh>().text = PlayerPrefs.GetInt("HighScore",0).ToString();
+    }
+}
